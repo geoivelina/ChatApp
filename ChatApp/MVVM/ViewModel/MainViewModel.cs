8a0b3404c76fd6c8e8296ac5f5adcaeeb19ce431
geoivelina <geoivelina@hotmail.com>
2024-01-26 08:46:42 +0200

    Add project files.

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using ChatApp.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            Messages.Add(new MessageModel
+            {
+                Username = "SomeName",
+                UsernameColor = "#409aff",
+                ImageSource = "https://tse1.mm.bing.net/th?id=OIP.dtqKdwsdEUpJauqb0Z4f_QHaJj&pid=Api",
+                Message = "Test message",
+                Time = DateTime.UtcNow,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "TestUser",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://tse1.mm.bing.net/th?id=OIP.dtqKdwsdEUpJauqb0Z4f_QHaJj&pid=Api",
+                    Message = "Test message",
+                    Time = DateTime.UtcNow,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "TestUser ",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://tse1.mm.bing.net/th?id=OIP.dtqKdwsdEUpJauqb0Z4f_QHaJj&pid=Api",
+                    Message = "Test message",
+                    Time = DateTime.UtcNow,
+                    IsNativeOrigin = true
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "TestUser",
+                UsernameColor = "#409aff",
+                ImageSource = "https://tse1.mm.bing.net/th?id=OIP.dtqKdwsdEUpJauqb0Z4f_QHaJj&pid=Api",
+                Message = "Last message",
+                Time = DateTime.UtcNow,
+                IsNativeOrigin = true
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"TestUser {i}",
+                    ImageSource = "https://tse1.mm.bing.net/th?id=OIP.dtqKdwsdEUpJauqb0Z4f_QHaJj&pid=Api",
+                    Messages = Messages
+                });
+            }
+        }
+    }
+}
---
+++ ChatApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ChatApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeBtnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeBtnClick(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+
+        }
+
+        private void CloseBtnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+    }
